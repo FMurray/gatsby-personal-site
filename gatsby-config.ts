256e80c076e3dcb33cb0105c0f0a93265b3d5a7a
Forrest Murray <FMurray@gmail.com>
2023-01-26 12:00:36 -0500

    homepage updates

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,7 +2,7 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `Gatsby Personal Site`,
+    title: `Forrest Murray`,
     siteUrl: `https://www.yourdomain.tld`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
@@ -28,7 +28,7 @@ const config: GatsbyConfig = {
         path: `${__dirname}/blog`,
       }
     },
-    "gatsby-transformer-sharp"
+    "gatsby-transformer-sharp",
   ]
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,10 @@ import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+    <Layout pageTitle="Home">
       <StaticImage
-        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+        alt="A man running a chain mortising machine in a workshop."
+        src="../images/chain_mortiser.png"
       />
     </Layout>
   )
